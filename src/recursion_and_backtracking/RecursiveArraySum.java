450f6c7c8e917686b58c16ba3e8f1a25c5cc9519
Atanas (home PC) <valkanovatanas@gmail.com>
2022-06-17 22:54:06 +0300

    created combinatorial_problems directory and added resources in it

--- src/recursion_and_backtracking/RecursiveArraySum.java
+++ src/recursion_and_backtracking/RecursiveArraySum.java
@@ -1,7 +1,12 @@
 package recursion_and_backtracking;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class RecursiveArraySum {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
     }
 }
