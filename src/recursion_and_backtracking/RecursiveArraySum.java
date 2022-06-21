94a8f3cf088b82033666fbdb125d8af036bd49e9
Atanas <valkanovatanas@gmail.com>
2022-06-21 13:29:16 +0300

    01.recursive array sum solution

--- src/recursion_and_backtracking/RecursiveArraySum.java
+++ src/recursion_and_backtracking/RecursiveArraySum.java
@@ -8,5 +8,14 @@ public class RecursiveArraySum {
         Scanner scanner = new Scanner(System.in);
         int[] array = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
+        System.out.println(sumArray(array, 0));
+    }
+
+    private static int sumArray(int[] array, int index) {
+        if(index == array.length - 1) {
+            return array[index];
+        }
+
+        return array[index] + sumArray(array, index + 1);
     }
 }
